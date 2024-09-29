cf16c725fa4e615127802b0d61a04ae4da29f9db
Vivek Sawant <vicky210490@gmail.com>
2024-09-29 17:35:32 +0530

    git init

---
+++ tests/todo-list-app.ts
@@ -0,0 +1,16 @@
+import * as anchor from "@coral-xyz/anchor";
+import { Program } from "@coral-xyz/anchor";
+import { TodoListApp } from "../target/types/todo_list_app";
+
+describe("todo-list-app", () => {
+  // Configure the client to use the local cluster.
+  anchor.setProvider(anchor.AnchorProvider.env());
+
+  const program = anchor.workspace.TodoListApp as Program<TodoListApp>;
+
+  it("Is initialized!", async () => {
+    // Add your test here.
+    const tx = await program.methods.initialize().rpc();
+    console.log("Your transaction signature", tx);
+  });
+});
